418e72df4636b79152990885792fe487ea1ebec1
info-arnav <info@arnavgupta.net>
2022-09-20 11:09:40 +0000

    changes

--- redirect/pages/_app.js
+++ redirect/pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import {useEffect} from "react"
 function MyApp({ Component, pageProps }) {
   useEffect(()=>{
-    window.location.href = "https://www.upes.ac.in/";
+    window.location.href = "https://www.itsdope.in/";
   },[])
   return  <div />
 }
